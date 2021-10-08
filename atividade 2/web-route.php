d5f1eb1e4448f89df94a654287cd38abaf69a7fb
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-10-08 15:33:26 -0300

    Delete atividade 2 directory

--- atividade 2/web-route.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Facades\Route;
-use app\Http\Controllers\clientecontroller;
-
-Route::get('/', function(){
-    return view('welcome');
-});
-Route::get('/clientes',[clientecontroller::class,'show']);
