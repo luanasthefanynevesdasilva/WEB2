6c717af764f793de62bb506aa60dd52d981ceb23
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-10-26 10:52:03 -0300

    Delete atividade IV/CODIGO/resources/views directory

--- atividade IV/CODIGO/resources/views/clientes/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Página de clientes</h1>
-
-
-@foreach ($clientes as $cliente)
-<ul>
-    <li> nome do cliente: {{$cliente->nome}};</li>
-    <li> debito do cliente {{$cliente->debito}}</li>
-</ul>
-@endforeach
