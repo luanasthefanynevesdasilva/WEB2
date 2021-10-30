2617b5e63f066c168ee37f092bac644a137a09fb
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-10-30 14:29:36 -0300

    Delete atividade IV/CODIGO directory

--- atividade IV/CODIGO/resources/views/entradas/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Página de entrada</h1>
-
-@foreach ($entradas as $entrada)
-<ul>
-    <li>FORNECEDOR: {{ $entrada->idfornecedor }}</li>
-    <li>VALOR TOTAL: R${{ $entrada->valortotal }}</li>
-    <li>DATA DA COMPRA: {{ $entrada->datacompra }}</li>
-</ul>
-@endforeach
--- atividade IV/CODIGO/resources/views/vendas/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Página dos Venda </h1>
-
-@foreach ($vendas as $venda)
-<ul>
-    <li>CLIENTE: {{ $venda->idcliente }}</li>
-    <li>VALOR TOTAL: R${{ $venda->valortotal }}</li>
-    <li>DATA DA VENDA: {{ $venda->datavenda }}</li>
-</ul>
-@endforeach
