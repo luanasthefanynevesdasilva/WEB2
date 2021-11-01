3c90269600bc4d31c8bd82bda454e0ac40774b35
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-11-01 20:21:30 -0300

    Add files via upload

---
+++ codigo#/database/migrations/2021_10_28_231415_create_produto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome', 60);
+            $table->float('valor');
+            $table->string('descriçao');
+            $table->float('desconto');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
