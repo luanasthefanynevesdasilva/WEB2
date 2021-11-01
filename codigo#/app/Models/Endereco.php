385a6fd9c1b9439b10dcef423e679bc4091ebe99
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-11-01 20:19:53 -0300

    Add files via upload

---
+++ codigo#/app/Models/Endereco.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Endereco extends Model
+{
+    use HasFactory;
+
+    protected $table = "endereco";
+
+    protected $fillable = ['logradouro', 'bairro', 'cidade', 'uf', 'idcliente'];
+
+    public function cliente(){
+        return $this->belongsTo(Cliente::class, 'idcliente', 'id');
+    }
+}
