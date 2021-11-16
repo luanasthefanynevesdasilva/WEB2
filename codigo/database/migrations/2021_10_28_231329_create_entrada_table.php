59decffe7d1b34cc1619fe33c66f0b15a784ed62
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-11-16 12:21:34 -0300

    Add files via upload

---
+++ codigo/database/migrations/2021_10_28_231329_create_entrada_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntradaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entrada', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->float('valortotal');
+            $table->unsignedBigInteger('idfornecedor');
+ 
+             $table->foreign('idfornecedor')->references('id')->on('fornecedor')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entrada');
+    }
+}
