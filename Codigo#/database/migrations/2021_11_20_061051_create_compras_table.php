c9609655cbd259feaefc5d8d4ae140a2f22fffdc
luanasthefanynevesdasilva <81384177+luanasthefanynevesdasilva@users.noreply.github.com>
2021-11-22 11:45:57 -0300

    Add files via upload

---
+++ Codigo#/database/migrations/2021_11_20_061051_create_compras_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fornecedor_id');
+            $table->double('valortotal', 8,2);
+            $table->timestamps();
+
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compras');
+    }
+}
